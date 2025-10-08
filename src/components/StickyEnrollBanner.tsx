2a73a79e9d24f7322f89ae32ab6c4dd459248b25
deepak <symentix.deepak@gmail.com>
2025-10-08 14:07:27 +0530

    `Removed commented out div and its contents, and removed a minus sign before the closing div tag`

--- src/components/StickyEnrollBanner.tsx
+++ src/components/StickyEnrollBanner.tsx
@@ -52,7 +52,7 @@ export default function StickyEnrollBanner() {
             <span className="text-gray-700">Live Online | English</span>
           </div>
 
-          {/* <div className="flex items-center gap-3">
+           <div className="flex items-center gap-3">
             <div className="w-5 h-5 rounded-full flex items-center justify-center flex-shrink-0" style={{backgroundColor: '#0a2b56'}}>
               <svg className="w-3 h-3 text-white" fill="currentColor" viewBox="0 0 20 20">
                 <path fillRule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clipRule="evenodd" />
@@ -62,7 +62,7 @@ export default function StickyEnrollBanner() {
               <span className="font-medium text-gray-900">Dates:</span>
               <span className="text-gray-700 ml-1">1st Nov - 19th Nov</span>
             </div>
-          </div> */}
+          </div> 
         </div>
 
         {/* Enroll Button */}
