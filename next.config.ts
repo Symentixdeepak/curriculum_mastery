acc756e60fd1384f44dd0f825152cafeabd924b3
deepak <symentix.deepak@gmail.com>
2025-10-08 11:33:09 +0530

    `Added images configuration to disable _next/image API optimization`

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+   images: {
+    unoptimized: true, // disables _next/image API
+  },
 };
 
 export default nextConfig;
